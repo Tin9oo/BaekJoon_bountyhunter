90bb2df62315ac7c5cb354f73b63da7564ec5ea0
Tin9oo <rlawnghks99@gmail.com>
2023-01-02 15:06:36 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main(void) {
-    int T, a, b;
-    cin >> T;
+    int T=6;
+    int a[]={8,13,8,18,8,13};
+    int b[]={4,19,10,18,25,16};
+
+    int first_contest[]={1,3,6,10,15,21};
+    int first_reward[]={500,300,200,50,30,10};
+    int second_contest[]={1,3,7,15,31};
+    int second_reward[]={512,256,128,64,32};
+    
     for(int i=0;i<T;++i){
-        cin >> a >> b;
-
-        cout << a << b;
-
+        for()
     }
 
     return 0;
